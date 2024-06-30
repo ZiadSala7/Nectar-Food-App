84263461a2ef7f946ea4d2ef622ea708db4734f6
Ziad Salah <zslah2800@gmail.com>
2024-07-01 00:46:56 +0300

    refactoring

---
+++ lib/features/sign up/presentation/views/widgets/login_button_navigator.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:nectar_app/constants.dart';
+import 'package:nectar_app/core/utils/app_routers.dart';
+import 'package:nectar_app/core/utils/styles.dart';
+import 'package:nectar_app/core/widgets/text_button.dart';
+
+class LoginButtonNavigator extends StatelessWidget {
+  const LoginButtonNavigator({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Already have an account?',
+          style: Styles.textStyle16.copyWith(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        TextButtonLogin(
+          color: commonColor,
+          descriptionTxt: 'Sign up',
+          onPressed: () {
+            GoRouter.of(context).push(AppRouters.loginView);
+          },
+        ),
+      ],
+    );
+  }
+}
--- lib/features/sign up/presentation/views/widgets/sign_up_view_body.dart
+++ lib/features/sign up/presentation/views/widgets/sign_up_view_body.dart
@@ -1,13 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:nectar_app/constants.dart';
 import 'package:nectar_app/core/utils/app_images.dart';
-import 'package:nectar_app/core/utils/app_routers.dart';
-import 'package:nectar_app/core/utils/styles.dart';
 import 'package:nectar_app/core/widgets/custom_button.dart';
 import 'package:nectar_app/core/widgets/custom_orange_carrot_img.dart';
-import 'package:nectar_app/core/widgets/text_button.dart';
+import 'package:nectar_app/features/sign%20up/presentation/views/widgets/custom_sign_up_buttons.dart';
 import 'package:nectar_app/features/sign%20up/presentation/views/widgets/custom_text_field_signup_section.dart';
+import 'package:nectar_app/features/sign%20up/presentation/views/widgets/login_button_navigator.dart';
 import 'package:nectar_app/features/sign%20up/presentation/views/widgets/sign_up_texts_section.dart';
 
 class SignUpViewBody extends StatelessWidget {
@@ -33,69 +30,12 @@ class SignUpViewBody extends StatelessWidget {
                 const CustomOrangeCarrotImg(),
                 const SignUpTextsSection(),
                 const CustomSignUpTextField(),
-                const SizedBox(
-                  height: 15,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        const SizedBox(
-                          width: 15,
-                        ),
-                        Text(
-                          'By continuing you agree to our ',
-                          style: Styles.textStyle14.copyWith(
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        TextButtonLogin(
-                          color: commonColor,
-                          descriptionTxt: 'Terms of Service',
-                          onPressed: () {
-                            // GoRouter.of(context).push(AppRouters.signUpView);
-                          },
-                        ),
-                        const Text(
-                          'and',
-                          style: Styles.textStyle14,
-                        ),
-                      ],
-                    ),
-                    TextButtonLogin(
-                      color: commonColor,
-                      descriptionTxt: ' Privacy Policy.',
-                      onPressed: () {
-                        // GoRouter.of(context).push(AppRouters.signUpView);
-                      },
-                    ),
-                  ],
-                ),
+                const CustomSignUpButtons(),
                 CustomButton(
                   descriptionButtonTxt: 'Sign in',
                   onPressed: () {},
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Already have an account?',
-                      style: Styles.textStyle16.copyWith(
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    TextButtonLogin(
-                      color: commonColor,
-                      descriptionTxt: 'Sign up',
-                      onPressed: () {
-                        GoRouter.of(context).push(AppRouters.signUpView);
-                      },
-                    ),
-                  ],
-                ),
+                const LoginButtonNavigator(),
               ],
             ),
           ),
