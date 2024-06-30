8e99134e0be640a1f71f810b7b7a02a233b067c5
Ziad Salah <zslah2800@gmail.com>
2024-06-30 11:23:27 +0300

    atallah's updates part two

--- lib/features/login/presentation/views/widgets/login_view_body.dart
+++ lib/features/login/presentation/views/widgets/login_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nectar_app/core/utils/app_images.dart';
 import 'package:nectar_app/features/login/presentation/views/widgets/login_body_items.dart';
 
 class LoginViewBody extends StatelessWidget {
@@ -6,9 +7,19 @@ class LoginViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(10),
-      child: LoginBodyItems(),
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          image: AssetImage(AppImages.backgroundImg),
+          fit: BoxFit.fitWidth,
+        ),
+      ),
+      child: const Padding(
+        padding: EdgeInsets.all(10),
+        child: LoginBodyItems(),
+      ),
     );
   }
 }
