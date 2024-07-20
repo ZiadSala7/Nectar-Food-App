ed1341fb06a4ee1a5db5065fb65087958b887f66
Ziad Salah <zslah2800@gmail.com>
2024-07-20 12:15:19 +0300

    search view edits part 2

--- lib/features/search/presentation/views/widgets/outline_input_border_method.dart
+++ lib/features/search/presentation/views/widgets/outline_input_border_method.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 OutlineInputBorder outlineInputBorderMethod() {
   return OutlineInputBorder(
     borderRadius: BorderRadius.circular(15),
-    borderSide: const BorderSide(width: 0.2),
+    borderSide: const BorderSide(width: 0.01),
   );
 }
