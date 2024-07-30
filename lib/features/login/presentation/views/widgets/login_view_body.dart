3513eceeab0c248d63299ba87685b4de6b007708
Ziad Salah <zslah2800@gmail.com>
2024-07-30 20:31:02 +0300

    form key

--- lib/features/login/presentation/views/widgets/login_view_body.dart
+++ lib/features/login/presentation/views/widgets/login_view_body.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:nectar_app/core/widgets/custom_background_box_decoration_img.dart';
 import 'package:nectar_app/core/widgets/custom_orange_carrot_img.dart';
-import 'package:nectar_app/features/login/data/model/login_controllers.dart';
 import 'package:nectar_app/features/login/presentation/views/widgets/buttons_section.dart';
 import 'package:nectar_app/features/login/presentation/views/widgets/text_field_section.dart';
 import 'package:nectar_app/features/login/presentation/views/widgets/text_section.dart';
@@ -11,30 +10,27 @@ class LoginViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: LoginControllers.loginFormKey,
-      child: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          decoration: backgroundBoxDecorationImg(),
-          child: const Padding(
-            padding: EdgeInsets.all(10),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                CustomOrangeCarrotImg(),
-                TextSection(),
-                SizedBox(
-                  height: 40,
-                ),
-                //TextField Data
-                TextFieldSection(),
-                SizedBox(
-                  height: 15,
-                ),
-                ButtonsSection(),
-              ],
-            ),
+    return SingleChildScrollView(
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        decoration: backgroundBoxDecorationImg(),
+        child: const Padding(
+          padding: EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              CustomOrangeCarrotImg(),
+              TextSection(),
+              SizedBox(
+                height: 40,
+              ),
+              //TextField Data
+              TextFieldSection(),
+              SizedBox(
+                height: 15,
+              ),
+              ButtonsSection(),
+            ],
           ),
         ),
       ),
